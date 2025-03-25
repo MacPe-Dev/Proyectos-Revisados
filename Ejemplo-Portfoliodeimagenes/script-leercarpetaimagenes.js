e0bc8002c372a4f42cbcbff4c6e65ba416503049
MacPe-Dev <139461691+MacPe-Dev@users.noreply.github.com>
2025-03-25 01:06:27 +0000

    dia240325_2

--- Ejemplo-Portfoliodeimagenes/script-leercarpetaimagenes.js
+++ Ejemplo-Portfoliodeimagenes/script-leercarpetaimagenes.js
@@ -1,29 +1,3 @@
-const imagenes = document.querySelectorAll('.img-galeria')
-const imagenesLight = document.querySelector('.agregar-imagen')
-const contenedorLight = document.querySelector('.img-light')
-const hamburguer1 = document.querySelector('.hamburger');
-
-imagenes.forEach(imagen =>{
-    imagen.addEventListener('click', ()=>{
-        aparecerImagen(imagen.getAttribute('src'))
-    })
-})
-
-contenedorLight.addEventListener('click', (e)=>{
-    if(e.target !==imagenesLight){
-        contenedorLight.classList.toggle('show')
-        imagenesLight.classList.toggle('showImage')
-        hamburguer1.style.opacity = '1'
-    }
-})
-
-const aparecerImagen = (imagen)=>{
-    imagenesLight.src = imagen
-    contenedorLight.classList.toggle('show')
-    imagenesLight.classList.toggle('showImage')
-    hamburguer1.style.opacity = '0'
-}
-
 // Función para obtener un número aleatorio entre min y max
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -87,3 +61,7 @@ close.addEventListener('click', () => {
 
 // Cargar las imágenes cuando el documento esté listo
 document.addEventListener('DOMContentLoaded', cargarImagenes);
+
+// Cargar Las imágenes cuando el documento esté listo
+// hay que cargar las imagenes desde una carpeta y que se muestren aleaotorimanete
+// y que al clickar en una imagen se muestre en grande.
